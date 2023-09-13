8ec2f27e58db1ab5266326994c291cb96625e77e
Himanshu221063 <144879574+Himanshu221063@users.noreply.github.com>
2023-09-13 12:15:38 +0530

    Add files via upload

---
+++ CheckEmptyString.java
@@ -0,0 +1,10 @@
+public class CheckEmptyString
+{
+	public static void main(String[] args) {
+		String s ="hello";
+		boolean b=s.isEmpty();
+		System.out.println(s.isEmpty());
+		
+		
+	}
+}
---
+++ EmptyorNotString.java
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class EmptyorNotString
+{
+	public static void main(String[] args) {
+		
+	String s="Hello";
+	System.out.println(s.charAt(3));
+	}
+}
