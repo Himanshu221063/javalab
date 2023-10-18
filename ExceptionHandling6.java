edb747a5e8c84e22b7336e70f3e16cb538e9634e
Himanshu221063 <144879574+Himanshu221063@users.noreply.github.com>
2023-10-18 12:39:31 +0530

    Add files via upload

---
+++ ExceptionHandling6.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class ExceptionHandling6
+{
+	public static void main(String[] args) {
+		try{
+		    int x=10;
+		    int y=0;
+		    int z=x/y;
+		    System.out.println(z);
+		}
+		catch(Exception e)
+		{
+		    System.out.println("Exception=> " + e);
+		}
+	}
+}
